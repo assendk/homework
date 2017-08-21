763033fde8aecb85a0a5e76a12437c8f2901b2ae
assendk <assendk@gmail.com>
2017-08-21 14:36:30 +0300

    added editor and viewer php

---
+++ L19/editor.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: assen.kovachev
+ * Date: 21.8.2017 г.
+ * Time: 01:13 ч.
+ */
---
+++ L19/viewer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: assen.kovachev
+ * Date: 21.8.2017 г.
+ * Time: 01:13 ч.
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="">
+    Product <br>
+    <input type="text">
+    <br>
+    Qty <br>
+    <input type="text">
+    <br>
+    <input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
