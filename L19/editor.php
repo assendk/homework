d00fdd48b87affad2004c7a061da9a548ec476dc
assendk <assendk@gmail.com>
2017-08-22 01:34:29 +0300

    edit

--- L19/editor.php
+++ L19/editor.php
@@ -6,9 +6,22 @@
  * Time: 01:13 ч.
  */
 require_once "config.php";
+$producti = [];
+$producti[] = "kakao - 4";
+$producti[] = "bob - 3";
+$producti[] = "zele - 5";
 $myFileName = $path . "myDB.txt";
-$myFile = fopen($myFileName, "a");
+$myFile = fopen($myFileName, "a") or die("can't open file");
+$clear = file_put_contents($myFileName, "");
+
+fwrite($myFile, json_encode($producti));
 fclose($myFile);
+
+$text = file_get_contents("db/myDB.txt");
+echo PHP_EOL;
+var_dump(json_decode($text, true));
+
+
 
 ?>
 <!doctype html>
