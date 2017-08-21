a1a3a4bc634eee109596e1402c2da278625688fe
assendk <assendk@gmail.com>
2017-08-21 15:50:38 +0300

    add files

--- L19/index.php
+++ L19/index.php
@@ -5,4 +5,18 @@
  * Date: 20.8.2017 г.
  * Time: 22:48 ч.
  */
-echo "Hello world";
+//echo "Hello world";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ L19/menu.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: assen.kovachev
+ * Date: 21.8.2017 г.
+ * Time: 14:43 ч.
+ */
+?>
+<div id="menu">
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="editor.php">Editor</a></li>
+    <li><a href="viewer.php">Viewer</a></li>
+</ul>
+</div>
